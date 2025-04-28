f8fb53a28c7e09b8db4915068ef596d237c8a988
Atharv625 <patilatharv625@gmail.com>
2025-04-29 01:10:56 +0530

    guessing number event management

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,19 +1,38 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
-const calcTemp=function(t1,t2)
-{  const temps=t1.concat(t2);
-    let max=temps[0];
-    let min=temps[0];
-    for(let i=0;i<temps.length;i++)
-    {
-        if(max<temps[i])
-            max=temps[i];
-        if(min>temps[i])
-            min=temps[i];
-    }
+// const calcTemp=function(t1,t2)
+// {  const temps=t1.concat(t2);
+//     let max=temps[0];
+//     let min=temps[0];
+//     for(let i=0;i<temps.length;i++)
+//     {
+//         if(max<temps[i])
+//             max=temps[i];
+//         if(min>temps[i])
+//             min=temps[i];
+//     }
 
-    console.log(max, min);
-    return max-min;
-};
-calcTemp([5,6,8,59],[89,36,45,25,1]);
+//     console.log(max, min);
+//     return max-min;
+// };
+// calcTemp([5,6,8,59],[89,36,45,25,1]);
+
+
+  const data1=[17,21,23]
+  const data2=[12,5,-5,0,4]
+  var temp="°C";
+  console.log(`...${data1[0]}${temp}...${data1[1]}${temp}...${data1[2]}${temp}`)
+
+
+  const printForecast=function (arr)
+  {
+    let str='';
+    for(let i=0;i<arr.length;i++)
+    {
+        str=str+` ${arr[i]}${temp}`
+
+    }
+    console.log(str);
+  };
+  printForecast(data1);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent="correct Number🔥";
+
+
+// document.querySelector(".number").textContent=13;
+// document.querySelector('.score').textContent=20;
+
+// document.querySelector(".guess").value=52;
+
+
+document.querySelector('.check').addEventListener('click',function(){
+ const guess=Number(document.querySelector('.guess').value);
+    // document.querySelector('.message').textContent="correct Number🔥";
+
+    console.log(guess,typeof guess);
+    if(!guess){
+        document.querySelector('.message').textContent="Please input Number ❌❌"
+    }
+
+});
