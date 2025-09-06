97148385560d6b9e082dcb6386dc50602600bdc2
Atharv625 <patilatharv625@gmail.com>
2025-09-06 23:58:55 +0530

    new module

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,6 @@
+import {addToCart,totalPrice, tq} from './shoppingCart.js';
+console.log('importing module')
+addToCart("condoms",10);
+addToCart("lubricant",20);
+
+console.log(totalPrice, totalQuantity)
